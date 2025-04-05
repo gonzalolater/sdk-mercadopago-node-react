ce508c7ec0849b0d6d912fcbea33b85d02508877
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2025-04-05 16:07:23 -0300

    mejorar formato y manejo de errores en el procesamiento de pagos

--- server/index.js
+++ server/index.js
@@ -11,37 +11,43 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.post("/process-payment", (req, res) => {
-    mercadopago.configurations.setAccessToken(process.env.ACCESS_TOKEN_MP);
-    const payment_data = {
-        transaction_amount: req.body.transaction_amount,
-        token: req.body.token,
-        description: req.body.description,
-        installments: Number(req.body.installments),
-        payment_method_id: req.body.paymentMethodId,
-        issuer_id: req.body.issuer,
-        payer: {
-            email: req.body.payer.email,
-            identification: {
-                type: req.body.payer.docType,
-                number: req.body.payer.docNumber,
-            },
-        },
-    };
+  mercadopago.configurations.setAccessToken(process.env.ACCESS_TOKEN_MP);
+  const payment_data = {
+    transaction_amount: req.body.transaction_amount,
+    token: req.body.token,
+    description: req.body.description,
+    installments: Number(req.body.installments),
+    payment_method_id: req.body.paymentMethodId,
+    issuer_id: req.body.issuer,
+    payer: {
+      email: req.body.payer.email,
+      identification: {
+        type: req.body.payer.docType,
+        number: req.body.payer.docNumber,
+      },
+    },
+  };
 
-    mercadopago.payment
-        .save(payment_data)
-        .then((response) => {
-            return res.status(response.status).json({
-                status: response.body.status,
-                status_detail: response.body.status_detail,
-                id: response.body.id,
-            });
-        })
-        .catch((err) => {
-            return res.status(500).send(err);
-        });
+  mercadopago.payment
+    .save(payment_data)
+    .then((response) => {
+      return res.status(response.status).json({
+        status: response.body.status,
+        status_detail: response.body.status_detail,
+        id: response.body.id,
+      });
+    })
+    .catch((err) => {
+      console.error("Payment error:", err);
+      return res
+        .status(500)
+        .json({
+          error: "An error occurred while processing the payment",
+          details: err,
+        }); // Incluye detalles del error (para depuración, considera no enviarlos en producción)
+    });
 });
 
 app.listen(port, () => {
-    console.log(`App running in http://localhost:${port}`);
+  console.log(`App running in http://localhost:${port}`);
 });
